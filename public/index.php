9af6681e406cdf59c902529c9b60116668ab3b1d
umudo <umutcang@gmail.com>
2017-01-23 13:21:59 +0300

    Move App autoloader to composer. Changed type to project in composer.json.

--- public/index.php
+++ public/index.php
@@ -2,30 +2,6 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-spl_autoload_register(function ($class) {
-    $prefix = 'App\\';
-    $base_dir = __DIR__ . '/../App/';
-
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        // no, move to the next registered autoloader
-        return false;
-    }
-
-    $relative_class = substr($class, $len);
-
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-
-    // if the file exists, require it
-    if (file_exists($file)) {
-        require $file;
-
-        return true;
-    }
-
-    return false;
-});
-
 date_default_timezone_set('Europe/Istanbul');
 setlocale(LC_ALL, 'en_US.UTF-8');
 mb_internal_encoding("UTF-8");
